675849f8d90b6356a2cbbb3c4273e172450a7fcb
mgritsch04 <Gritsch.Maximilian@sus.htl-grieskirchen.at>
2021-09-28 15:36:28 +0200

    10. commit

--- src/net/htlgkr/mgritsch19/bsp3/NumberTester.java
+++ src/net/htlgkr/mgritsch19/bsp3/NumberTester.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package bsp3;
+package net.htlgkr.mgritsch19.bsp3;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
---
+++ src/net/htlgkr/mgritsch19/bsp4/AbstraktCalculator.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.htlgkr.mgritsch19.bsp4;
+
+import net.htlgkr.mgritsch19.bsp4.Number.CalculationOperation;
+
+/**
+ *
+ * @author maxim
+ */
+public abstract class AbstraktCalculator {
+
+    protected CalculationOperation add;
+    protected CalculationOperation subtract;
+    protected CalculationOperation multiply;
+    protected CalculationOperation divide;
+
+    public AbstraktCalculator(CalculationOperation add, CalculationOperation subtract, CalculationOperation multiply, CalculationOperation divide) {
+        this.add = add;
+        this.subtract = subtract;
+        this.multiply = multiply;
+        this.divide = divide;
+    }
+
+    public abstract Number add(Number a, Number b);
+
+    public abstract Number subtract(Number a, Number b);
+
+    public abstract Number multiply(Number a, Number b);
+
+    public abstract Number divide(Number a, Number b);
+}
