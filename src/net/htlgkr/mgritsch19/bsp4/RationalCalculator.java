85d576329e2d87568206fafd766c29c54957bac5
mgritsch04 <Gritsch.Maximilian@sus.htl-grieskirchen.at>
2021-09-28 15:55:26 +0200

    12. commit

--- src/net/htlgkr/mgritsch19/bsp4/RationalCalculator.java
+++ src/net/htlgkr/mgritsch19/bsp4/RationalCalculator.java
@@ -11,9 +11,13 @@ package net.htlgkr.mgritsch19.bsp4;
  */
 public class RationalCalculator extends AbstraktCalculator {
 
+    public interface CalculationOperation {
+
+        Number calc(Number x, Number y);
+    }
+
     @Override
     public Number add(Number a, Number b) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
