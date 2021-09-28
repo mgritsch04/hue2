ab4610681fc3988fa636b0055d9d09fbf00dbaeb
mgritsch04 <Gritsch.Maximilian@sus.htl-grieskirchen.at>
2021-09-28 15:38:09 +0200

    11. commit

---
+++ src/net/htlgkr/mgritsch19/bsp4/RationalCalculator.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.htlgkr.mgritsch19.bsp4;
+
+/**
+ *
+ * @author maxim
+ */
+public class RationalCalculator extends AbstraktCalculator {
+
+    @Override
+    public Number add(Number a, Number b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Number subtract(Number a, Number b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Number multiply(Number a, Number b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Number divide(Number a, Number b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
