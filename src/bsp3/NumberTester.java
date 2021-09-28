90763ac09558f1feb2be5d066ddbe27570554ad4
mgritsch04 <Gritsch.Maximilian@sus.htl-grieskirchen.at>
2021-09-28 15:30:49 +0200

    9. commit

--- src/bsp3/NumberTester.java
+++ src/bsp3/NumberTester.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package net.htlgkr.mgritsch19.hue2;
+package bsp3;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
---
+++ src/net/htlgkr/mgritsch19/bsp4/Number.java
@@ -0,0 +1,38 @@
+package net.htlgkr.mgritsch19.bsp4;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author maxim
+ */
+public class Number {
+
+    private double a;
+    private double b;
+
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public interface CalculationOperation {
+
+        Number calc(Number x, Number y);
+    }
+}
